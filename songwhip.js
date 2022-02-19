2afe06d1ada2104d171e181c3580410dc1b3163b
Luca Z <l-zeuch@email.de>
2022-02-19 01:25:12 +0100

    decor: use modal and let user copy the link

--- songwhip.js
+++ songwhip.js
@@ -5,48 +5,91 @@
 // DESCRIPTION: Get the song/artist URL from https://songwhip.com
 
 (function Songwhip() {
-	const SW_TEXT = "Songwhip!";
+	const SW_TEXT = "Songwhip!"
+	const SW_URL = "https://songwhip.com"
 
-	if (!Spicetify.Player.data) {
-		setTimeout(Songwhip, 1000);
-		return;
+	if (!document.body.classList.contains('songwhip-css')) {
+		var styleSheet = document.createElement("style")
+
+		styleSheet.innerHTML =
+		`.sw-par {
+		clear: right !important;
+		margin-bottom: 1em !important;
+		}`
+
+		document.body.appendChild(styleSheet)
+		document.body.classList.add('songwhip-css')
 	}
 
+	if (!(Spicetify.CosmosAsync && Spicetify.Player.data && Spicetify.URI)) {
+		setTimeout(Songwhip, 1000)
+		return
+	}
+
+
+	function error() {
+		Spicetify.PopupModal.display({
+			title: "Error",
+			content: "Failed fetching Songwhip :("
+		})
+	}
+
+
 	/**
+	* Return the found songwhip JSON stuff
 	* @param {string[]} uris
+	* @returns {string}
 	*/
 	async function getSongwhip(uris) {
-		const SW_URL = "https://songwhip.com";
-		const uri = uris[0];
-		const data = await Spicetify.CosmosAsync.post(SW_URL, JSON.stringify({ url: uri }));
+		const uri = uris[0]
+		const body = JSON.stringify({ url: uri })
 
-		console.log("Got " + data.url);
+		const data = await Spicetify.CosmosAsync.post(SW_URL, body)
+		if (!data) {
+			error()
+		}
+
+		makeModal(data)
 	}
 
 	/**
-	* @param {string[]} uris
-	* @returns {boolean}
+	*  Create the popup modal
+	*  @param {string}
 	*/
-	function shouldAddContextMenu(uris) {
-		if (uris.length > 1) {
-			return false;
+	function makeModal(data) {
+		const description = data.artists[0].description
+		if (description.length > 280) {
+			description.slice(0, 277)
+			description.concat("...")
 		}
 
-		const uri = uris[0];
-		const uriObj = Spicetify.URI.fromString(uri);
-		if (uriObj.type === Spicetify.URI.Type.TRACK) {
-			this.name = SW_TEXT;
-			return true;
-		}
+		const title = `<a href=${data.url}>${data.name} -- ${data.artists.map(a => a.name).join(', ')}</a>`
+		const content = `<p class="sw-par">${data.url}</p>
+		<p class="sw-par">${description}</p>
+		`
 
-		if (uriObj.type === Spicetify.URI.Type.ARTIST) {
-			this.name = SW_TEXT;
-			return true;
-		}
-
-		return false;
+		Spicetify.PopupModal.display({
+			title: title,
+			content: content
+		})
 	}
 
-	const cntxMenu = new Spicetify.ContextMenu.Item(SW_TEXT, getSongwhip, shouldAddContextMenu);
-	cntxMenu.register();
+	function shouldAddContextMenu(uris) {
+		if (uris.length > 1) {
+			return false
+		}
+
+		let uri = uris[0]
+		uri = Spicetify.URI.fromString(uri)
+		if (uri.type === Spicetify.URI.Type.TRACK || uri.type === Spicetify.URI.Type.ARTIST) {
+			this.name = SW_TEXT
+			return true
+		}
+
+		return false
+	}
+
+	// Register the button thingy
+	const cntxMenu = new Spicetify.ContextMenu.Item(SW_TEXT, getSongwhip, shouldAddContextMenu)
+	cntxMenu.register()
 })();
